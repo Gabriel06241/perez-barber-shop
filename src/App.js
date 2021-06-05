e6b95f840ce3a65768fd55799ab01cdd486e590e
Gabriel Perez Moreno <gabriel.perez@chiper.co>
2021-06-05 17:09:30 -0500

    feat: updating master branch!

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './coderhouse.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to <code>Perez Barber Shop!</code>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
