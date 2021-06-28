d2204e3c1f210d35163870f48efb1cfbf8fb55f2
Gabriel Perez Moreno <gabriel.perez@chiper.co>
2021-06-27 23:00:01 -0500

    feat: adding item count component!

--- src/components/itemListContainer/itemListContainer.js
+++ src/components/itemListContainer/itemListContainer.js
@@ -1,6 +1,10 @@
 import './itemListContainer.css';
+import { ItemCount } from './../itemCount/itemCount'
 export const ItemListContainer = ({ greeting }) => {
   return (
-    <div className='item-list-container'>{greeting}</div>
+    <div className='item-list-container'>
+      <p>{greeting}</p>
+      <ItemCount stock="5" initial="1" />
+    </div>
   )
 }
